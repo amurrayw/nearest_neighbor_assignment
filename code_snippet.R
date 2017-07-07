a8b1335cfc52efd2db07bb8e5862f0978a08ce40
amurrayw <swiftwind1230@gmail.com>
2017-07-07 14:50:32 +0200

    Cleaned up typos.

--- code_snippet.R
+++ code_snippet.R
@@ -1,6 +1,29 @@
 ###################TODO: Need to restrict the used.neighbors vector to only include the 65 variables from the sample.
 ############We don't care if a nearest neighbor has been used up by a variable outside of the sample.
 
+
+
+## Function: assign.neighbors() = Assigns unique nearest neighbors.
+
+## Inputs:
+
+## in.sample either a Boolean which says to not run the
+## procedure on a specified subset of the NN matrix, or a vector
+## specifying which row positions to include from the NN matrix.
+
+## NN = the nearest neighbor matrix.
+
+## rand = a Boolean specifying if
+## neighbor conflict resolution should be done in a random order.
+
+## Sample.indx = specifies which variables were part of the sample, and
+## so shouldn't be used to fill in nearest neighbors.
+
+## Outputs: Returns a vector containing the resulting neighbor
+## assignments. The position of each number specifies the variable
+## receiving the nearest neighbor. The vector can also include NAs due
+## to an inability to find an unused nearest neighbor to assign.
+
 assign.neighbors <- function(in.sample=FALSE, NN, rand=FALSE, sample.indx=which(s>0)){
 
     if(!is.logical(in.sample)){
@@ -28,44 +51,37 @@ assign.neighbors <- function(in.sample=FALSE, NN, rand=FALSE, sample.indx=which(
     ## A 0 denotes a variable with no assigned neighbor.
     assignment.vec <- rep.int(0, times=nrow(nearest.neigh.in.samp))
 
-    print("used.neighbors")
-    print((used.neighbors))
-
-    print("positions.filled")
-    print((positions.filled))  
-
     assignment.vec[positions.filled] <- used.neighbors
 
     #Forbids sample from being a neighbor.
     used.neighbors <- unique(c(used.neighbors, sample.indx))
-        
+    
     ## Randomly reorders order of conflict resolution if desired.
     if(rand==TRUE){
         need.resolved <- need.resolved[sample(1:length(need.resolved), size=length(need.resolved))]
     }
 
-
-    print("need.resolved")
-    print(need.resolved)
-    
     return(resolve.conflict(need.resolved, used.neighbors,
                             nearest.neigh.in.samp, nearest.neigh.dist, assignment.vec))
 }
 
 
+##Function: resolve.conflict() = Recursively resolves conflicts due to duplicate nearest neighbors.
+
+## Inputs: 
+## Outputs:Vector containing assignments.
+
+
 resolve.conflict <- function(need.resolved, used.neighbors, nearest.neigh.in.samp,
                              nearest.neigh.dist, assignment.vec){
 
-    if(length(need.resolved>0)){
+    if(length(need.resolved)>0){
 
         currently.resolving <- need.resolved[1]
         nearest.neighbors <- nearest.neigh.in.samp[currently.resolving,]
         
         ## Selects first matching nearest neighbor not yet used.
         to.assign <-nearest.neighbors[!(nearest.neighbors %in% used.neighbors)][1]
-
-
-#        print(nearest.neighbors[!(nearest.neighbors %in% used.neighbors)][1])
         
         ## Assigning selected nearest neighbor.
         assignment.vec[currently.resolving] <- to.assign
@@ -85,22 +101,30 @@ resolve.conflict <- function(need.resolved, used.neighbors, nearest.neigh.in.sam
 }
 
 
-
+## Runs test cases to make sure function actually works.
 run.test.cases <- function(){
-    test.case <- cbind(c(1,3,3,2,5), c(3,2,4,5,1))
+    test.case.1 <- cbind(c(1,3,3,2,5), c(3,2,4,5,1))
 
-    test.1.result <- all.equal(c(1,3,4,2,5), assign.neighbors(NN=list(nn.index=test.case,
-                                                            nn.dist=test.case), sample.indx=NULL))
+    test.1.result <- all.equal(c(1,3,4,2,5), assign.neighbors(NN=list(nn.index=test.case.1,
+                                                            nn.dist=test.case.1), sample.indx=NULL))
+
+
+    test.case.2 <- cbind(c(1,2,3), c(3,2,1))
+
+    test.2.result <- all.equal(c(), assign.neighbors(NN=list(nn.index=test.case.2,
+                                                            nn.dist=test.case.2), sample.indx=1))
+
     
-    return(list(test.1.result))
+    return(list(test.1.result=test.1.result, test.2.result=test.2.result ))
     
 }
 
 run.test.cases()
 
+
+## Actually running the code on the data set.
+
 load(file="NN.RData")
 
-
-
-resulting.assingment <- assign.neighbors(NN=NN, sample.indx=NULL)
+resulting.assignment <- assign.neighbors(NN=NN, sample.indx=NULL)
 
